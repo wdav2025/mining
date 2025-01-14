062a999b46384a4dbf05592082ab8a7f2a3dce08
William Davis <williamdav@protonmail.com>
2025-01-14 11:57:48 -0500

    Change comment

--- PubMedAPIsearch.sh
+++ PubMedAPIsearch.sh
@@ -1,6 +1,6 @@
 # Pubmed API info: https://www.ncbi.nlm.nih.gov/books/NBK25499/#_chapter4_ESearch_
 
-# Search two terms using + sign
+# Search boolean terms
 curl -s "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&term=\
 cochlea+AND+utricle+OR\
 +semicircular+canal\
