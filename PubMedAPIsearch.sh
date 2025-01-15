42b019ed58319a78de3b266a089a559ad12bc00c
C57B6J <mrgilberto@gmail.com>
2025-01-15 17:11:15 -0500

    Update PubMedAPIsearch.sh

--- PubMedAPIsearch.sh
+++ PubMedAPIsearch.sh
@@ -1,10 +1,60 @@
 # Pubmed API info: https://www.ncbi.nlm.nih.gov/books/NBK25499/#_chapter4_ESearch_
 
-# Search boolean terms
+# Search boolean terms for all iner ear related papers (need to adjust total output "retmax" to by incremental ranges )
 curl -s "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&term=\
-cochlea+AND+utricle+OR\
-+semicircular+canal\
+cochlea+OR\+semicircular+canal+OR\+basilar+papilla+OR\
++vestibular+macula+OR\
++otolith+OR\
++crista+ampullaris+OR\
++organ+of+corti+OR\
++utricle+saccule+NOT\
++prostatic\
 &retstart=0\
 &retmax=60\
-&tool=biomed3&\
-retmode=json"
+&tool=biomed3&retmode=json"
+
+#Return Cochlea Hits Only 
+curl -s "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&term=\
+cochlea+OR\+basilar+papilla+OR\
++organ+of+corti+NOT\
++utricle+saccule+NOT\
++vestibular+macula+NOT\
++otolith+NOT\
++crista+ampullaris+NOT\
++semicircular+canal+NOT\
++prostatic\
+&retstart=0\
+&retmax=60\
+&tool=biomed3&retmode=json"
+
+#Return Vestibular Hits Only 
+curl -s "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&term=\
++semicircular+canal+OR\
++vestibular+macula+OR\
++otolith+OR\
++crista+ampullaris+OR\
++utricle+saccule+NOT\
++prostatic+NOT\
+cochlea+NOT\
++basilar+papilla+NOT\
++organ+of+corti+NOT\
+&retstart=0\
+&retmax=60\
+&tool=biomed3&retmode=json"
+
+#Date ranges (not yet specified for cmd...)
+curl -s "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&term=\
++semicircular+canal+OR\
++vestibular+macula+OR\
++otolith+OR\
++crista+ampullaris+OR\
++utricle+saccule+NOT\
++prostatic+NOT\
+cochlea+NOT\
++basilar+papilla+NOT\
++organ+of+corti+NOT\
+&datetype=pdat\
+&mindate=1825%2F01%2F01\
+&retstart=0\
+&retmax=60\
+&tool=biomed3&retmode=json"
