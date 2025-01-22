7e7ddcc27bfe2a8309c07b1a03e1e909645215f3
William Davis <william.davis@childrens.harvard.edu>
2025-01-22 14:41:01 -0500

    Add example of Literature Citation Exporter API

--- PubMedAPIsearch.sh
+++ PubMedAPIsearch.sh
@@ -2,9 +2,20 @@
 
 # Search boolean terms
 curl -s "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&term=\
-cochlea+AND+utricle+OR\
-+semicircular+canal\
-&retstart=0\
-&retmax=60\
+cochlea+OR+utricle\
+&datetype=pdat\
+&mindate=1800/01/01\
+&maxdate=3000/01/01\
+&dbfrom=pubmed\
 &tool=biomed3&\
-retmode=json"
+&retmode=json"
+
+
+# # Search boolean terms
+# curl -s "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&term=\
+# +covid19\
+# &datetype=edat\
+# &mindate=1880\
+# &maxdate=2015\
+# &tool=biomed3&\
+# &retmode=json"
