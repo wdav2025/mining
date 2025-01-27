d3cc54e98309be935a0a7c1921d7cbfce6a8995c
C57B6J <mrgilberto@gmail.com>
2025-01-27 14:12:41 -0500

    Create NIHReporterAPIsearch.sh
    First draft

---
+++ NIHReporterAPIsearch.sh
@@ -0,0 +1,20 @@
+#Search the NIHReporter Database for All Training Grants
+{
+  "criteria":{ advanced_text_search: { operator: "and",
+search_field: "projecttitle,abstracttext,terms", "search_text": "brain
+disorder" 
+  {
+"funding_mechanism":["F","K","T","R", "U"],
+"newly_added_projects_only": false,
+},
+"include_fields": [
+"FiscalYear","ProjectNum","Organization", "OrganizationType",
+"AwardType", "ActivityCode", "AwardAmount", "PrincipalInvestigators", 
+"AgencyIcAdmin", "AgencyIcFundings", "AwardNoticeDate", "CoreProjectNum", "ProjectTitle",
+"BudgetStart",,"DirectCostAmt","IndirectCostAmt"
+],
+"offset":0,
+"limit":25,
+"sort_field":"project_start_date",
+"sort_order":"desc"
+}
