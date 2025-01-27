53e11d55d596a5dfc3b6377c003676cfd153b54b
C57B6J <mrgilberto@gmail.com>
2025-01-27 11:30:24 -0500

    Update PubMedAPIsearch.sh
    Corrected search terms, added date ranges, and tested output which looks quantitatively correct with the exception of the boolean operators for publications that are "both auditory AND vestibular" which give inflated numbers larger than the total inner ear research filter.

--- PubMedAPIsearch.sh
+++ PubMedAPIsearch.sh
@@ -1,59 +1,50 @@
 # Pubmed API info: https://www.ncbi.nlm.nih.gov/books/NBK25499/#_chapter4_ESearch_
 
-# Search boolean terms for all iner ear related papers (need to adjust total output "retmax" to by incremental ranges )
+#Pull UIDs for all anatomical terms relating to the inner ear from 1825 to 2025, and exclude non-ear related terms like protstatic utricle and laryngeal saccule (63611 hits)
 curl -s "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&term=\
-cochlea+OR\+semicircular+canal+OR\+basilar+papilla+OR\
-+vestibular+macula+OR\
-+otolith+OR\
-+crista+ampullaris+OR\
-+organ+of+corti+OR\
-+utricle+saccule+NOT\
-+prostatic\
-&retstart=0\
-&retmax=60\
-&tool=biomed3&retmode=json"
-
-#Return Cochlea Hits Only 
-curl -s "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&term=\
-cochlea+OR\+basilar+papilla+OR\
-+organ+of+corti+NOT\
-+utricle+saccule+NOT\
-+vestibular+macula+NOT\
-+otolith+NOT\
-+crista+ampullaris+NOT\
-+semicircular+canal+NOT\
-+prostatic\
-&retstart=0\
-&retmax=60\
-&tool=biomed3&retmode=json"
-
-#Return Vestibular Hits Only 
-curl -s "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&term=\
-+semicircular+canal+OR\
-+vestibular+macula+OR\
-+otolith+OR\
-+crista+ampullaris+OR\
-+utricle+saccule+NOT\
-+prostatic+NOT\
-cochlea+NOT\
-+basilar+papilla+NOT\
-+organ+of+corti+NOT\
-&retstart=0\
-&retmax=60\
-&tool=biomed3&retmode=json"
-
-#Date ranges (not yet specified for cmd...)
-curl -s "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&term=\
-cochlea+OR\+semicircular+canal+OR\+basilar+papilla+OR\
-+vestibular+macula+OR\
-+otolith+OR\
-+crista+ampullaris+OR\
-+organ+of+corti+OR\
-+utricle+saccule+NOT\
-+prostatic\
+cochlea+OR\+basilar+papilla+OR\+organ+of+corti+OR\+stria+vascularis+OR\+basilar+membrane+OR\+reissner's+membrane+OR\+spiral+ganglion+OR\
+\vestibular+labyrinth+OR\+semicircular+canal+OR\+vestibular+macula+OR\+otolith+OR\+crista+ampullaris+OR\+utricle+OR\+saccule+NOT\
++prostatic+NOT\+laryngeal\
 &datetype=pdat\
 &mindate=1825\
 &maxdate=2025\
 &retstart=0\
 &retmax=60\
 &tool=biomed3&retmode=json"
+
+#Return Cochlear/Auditory hits only, but exclude all vestibular (34,320 hits)
+curl -s "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&term=\
+cochlea+OR\+basilar+papilla+OR\+organ+of+corti+OR\+stria+vascularis+OR\+basilar+membrane+OR\+reissner's+membrane+OR\+spiral+ganglion+NOT\
+\vestibular+labyrinth+NOT\+semicircular+canal+NOT\+vestibular+macula+NOT\+otolith+NOT\+crista+ampullaris+NOT\+utricle+NOT\+saccule+NOT\
++prostatic+NOT\+laryngeal\
+&datetype=pdat\
+&mindate=1825\
+&maxdate=2025\
+&retstart=0\
+&retmax=60\
+&tool=biomed3&retmode=json"
+
+#Return Vestibular Hits Only (27,282 hits)
+curl -s "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&term=\
+\vestibular+labyrinth+OR\+semicircular+canal+OR\+vestibular+macula+OR\+otolith+OR\+crista+ampullaris+OR\+utricle+OR\+saccule+NOT\
+cochlea+NOT\+basilar+papilla+NOT\+organ+of+corti+NOT\+stria+vascularis+NOT\+basilar+membrane+NOT\+reissner's+membrane+NOT\+spiral+ganglion+NOT\
+\+prostatic+NOT\+laryngeal\
+&datetype=pdat\
+&mindate=1825\
+&maxdate=2025\
+&retstart=0\
+&retmax=60\
+&tool=biomed3&retmode=json"
+
+#Pull UIDs for all auditory/Cochlear AND vestibular terms 1825 to 2025, and exclude non-ear related terms like protstatic utricle and laryngeal saccule (17514 hits)
+curl -s "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&term=\
+cochlea+OR\+basilar+papilla+OR\+organ+of+corti+OR\+stria+vascularis+OR\+basilar+membrane+OR\+reissner's+membrane+OR\+spiral+ganglion+AND\
+\vestibular+labyrinth+OR\+semicircular+canal+OR\+vestibular+macula+OR\+otolith+OR\+crista+ampullaris+OR\+utricle+OR\+saccule+NOT\
++prostatic+NOT\+laryngeal\
+&datetype=pdat\
+&mindate=1825\
+&maxdate=2025\
+&retstart=0\
+&retmax=60\
+&tool=biomed3&retmode=json"
+
