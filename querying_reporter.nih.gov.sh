4cba6e230e26c75e0a761d10a657558a84caabb5
William Davis <william.davis@childrens.harvard.edu>
2025-01-27 14:42:01 -0500

    Add functions and payload.json for querying JSON API of reporter.nih.gov

---
+++ querying_reporter.nih.gov.sh
@@ -0,0 +1,16 @@
+# Two functions for querying the JSON endpoint of api.reporter.nih.gov - one from the CLI and one from a file: payload.json
+#
+#  exmple use:
+#     . querying_reporter.nih.gov.sh      #  . means source, which loads this script into the shell environment so you can run from_file() or inline()
+#     from_file
+inline () {
+curl -qX POST https://api.reporter.nih.gov/v2/projects/search \
+  -H "Content-Type: application/json" \
+  -d '{"key1":"value1", "key2":"value2"}'
+}
+
+from_file () {
+curl -qX POST https://api.reporter.nih.gov/v2/projects/search \
+  -H "Content-Type: application/json" \
+  -d @payload.json
+}
