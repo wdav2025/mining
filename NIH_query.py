625c80e040569c405b74ebb29d306307a585e005
William Davis <william.davis@childrens.harvard.edu>
2025-03-06 11:52:49 -0500

    something

--- NIH_query.py
+++ NIH_query.py
@@ -1,21 +1,43 @@
 import json
 import requests
 import time
+import sqlite3
 
 endpoint = "https://api.reporter.nih.gov/v2/projects/search"
 
+# make the database --------------------------------
+database = sqlite3.connect("response_data.db")
+with open('schema.sql', 'r') as f:
+    database.executescript(f.read())
+# make the database --------------------------------
+
+
 query_payload = {
-    "criteria": {
-        "advanced_text_search": { "search_field": "terms,projecttitle", "search_text": "(cochlea OR \"basilar papilla\" OR \"organ of corti\" OR \"stria vascularis\" OR \"basilar membrane\" OR \"vestibular labyrinth\" OR \"semicircular canal\" OR \"vestibular macula\" OR otolith OR crista ampullaris OR utricle)"}
-    },
-    "include_fields": [
+        "criteria": {
+            "advanced_text_search": { "search_field": "terms,projecttitle", "search_text": "(cochlea OR \"basilar papilla\" OR \"organ of corti\" OR \"stria vascularis\" OR \"basilar membrane\" OR \"vestibular labyrinth\" OR \"semicircular canal\" OR \"vestibular macula\" OR otolith OR crista ampullaris OR utricle)"}
+            },
+        "include_fields": [
             "FiscalYear", "ProjectNum"
-    ],
-    "offset": 0,
-    "limit": 10,
-    "sort_field": "appl_id",
-    "sort_order": "desc"
- }
+            ],
+        "offset": 0,
+        "limit": 10,
+        "sort_field": "appl_id",
+        "sort_order": "desc"
+        }
+
+
+query_payload = {
+        "criteria": {
+            "advanced_text_search": { "search_field": "terms,projecttitle", "search_text": "(cochlea OR \"basilar papilla\" OR \"organ of corti\" OR \"stria vascularis\" OR \"reissner membrane\" OR \"spiral ganglion\" OR \"basilar membrane\" OR \"vestibular labyrinth\" OR \"semicircular canal\" OR \"vestibular macula\" OR \"otolith\" OR \"crista ampullaris\" OR  \"utricle\" NOT \"prostatic\" NOT \"pharyngeal\")"}
+            },
+        "include_fields": [
+            "FiscalYear", "ProjectNum", "ActivityCode","AwardType","Organization", "AwardAmount", "AbstractText"
+            ],
+        "offset": 0,
+        "limit": 10,   # should be 500
+        "sort_field": "project_start_date",
+        "sort_order": "desc"
+        }
 
 def make_request(q):
     r = requests.post(endpoint, json=q)
@@ -28,7 +50,7 @@ def make_request(q):
     #total_results = r.json()['meta']['total']
 
 
-    #time.sleep(2)
+    time.sleep(2)
 
 #print(json.dumps(r.json()))
 
@@ -37,14 +59,15 @@ def make_request(q):
 total_results = 90
 step = 10
 
-for i in range(0, ((total_results + step ) // step) * step, step ):
+#for i in range(0, ((total_results + step ) // step) * step, step ):
+for i in range(1):
 
     query_payload['offset'] = i
-    print(query_payload['offset'])
 
     r = make_request(query_payload)
+    print(json.dumps(r.json()))
 
 
-    with open("data.json", "a+") as outfile:
-        outfile.write(json.dumps(r.json()))
-    time.sleep(6)
+ #  with open("data.json", "a+") as outfile:
+ #      outfile.write(json.dumps(r.json()))
+ #  time.sleep(6)
